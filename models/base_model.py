e373d1e7908f7254ec757f7304b5b37e697817c1
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-09-28 20:21:45 +0100

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -59,7 +59,7 @@ class BaseModel:
         models.storage.save()
 
     def to_dict(self, save_fs=None):
-        """returns a dictionary containing all keys/values of the instance"""
+        """ Returns a dictionary containing all keys/values of the instance """
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
@@ -74,5 +74,5 @@ class BaseModel:
         return new_dict
 
     def delete(self):
-        """delete the current instance from the storage"""
+        """ Delete the current instance from the storage """
         models.storage.delete(self)
